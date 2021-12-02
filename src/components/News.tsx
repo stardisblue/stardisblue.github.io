7f5d1ac352e3aae396b54344285a9c97c5bd6ead
stardisblue <stardisblue@users.noreply.github.com>
2021-12-02 11:44:50 +0100

    updating width to remove scrollbar

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -5,7 +5,7 @@ export const News: React.FC = () => (
   <Section className="noprint" title="📰 NEWS">
     <iframe
       width="100%"
-      height="313"
+      height="319"
       frameBorder="0"
       src="https://observablehq.com/embed/@stardisblue/showcase?cells=viewof+showcase"
     ></iframe>
