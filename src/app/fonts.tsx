6813ae59cf7edb05b8f3460a139d2ea192459ce4
stardisblue <stardisblue@gmail.com>
2024-08-29 16:24:24 +0200

    style: set default fonts

---
+++ src/app/fonts.tsx
@@ -0,0 +1,17 @@
+import { Inter, Noto_Serif, Fira_Code } from 'next/font/google';
+
+export const inter = Inter({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+export const notoSerif = Noto_Serif({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-noto-serif',
+});
+export const firaCode = Fira_Code({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-fira-code',
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import classNames from 'classnames';
+import { firaCode, inter, notoSerif } from './fonts';
 
 export const metadata: Metadata = {
   title: 'Fati Chen',
@@ -16,7 +15,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={classNames(
+          firaCode.variable,
+          notoSerif.variable,
+          inter.className
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
