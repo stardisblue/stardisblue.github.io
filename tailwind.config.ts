8317a21606e47a76d5d4862f1b80558607f25f7e
stardisblue <stardisblue@gmail.com>
2024-09-03 23:53:40 +0200

    refactor: optimize tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from 'tailwindcss';
 import typography from '@tailwindcss/typography';
+
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -22,4 +22,5 @@ const config: Config = {
   },
   plugins: [typography],
 };
+
 export default config;
