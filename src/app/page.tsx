5e2cdf5650949f6c084867f627bce2570407b092
stardisblue <stardisblue@gmail.com>
2024-09-04 22:33:44 +0200

    style: nationwide prose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,56 +15,56 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <main id="CF" className="ph2-m container mx-auto divide-y">
-      <header className="py-4">
-        <h1>Fati CHEN</h1>
-        <h2>Docteur en Informatique, Data Science & Data Viz</h2>
-        <div>
-          <Link href="//stardis.blue">stardis.blue</Link>
-          {' · '}
-          <Link href="mailto:chen.fati@gmail.com" icon={Email}>
-            chen.fati@gmail.com
-          </Link>
-          {' · '}
-          <GithubLink href="stardisblue">stardisblue</GithubLink>
-          {' · '}
-          <ObservableLink href="@stardisblue">@stardisblue</ObservableLink>
-          {' · '}
-          <LinkedInLink href="in/fati-chen/">Fati Chen</LinkedInLink>
-        </div>
-      </header>
-      <Section>
-        <h2>📰 News</h2>
-        <iframe
-          width="100%"
-          height="319"
-          frameBorder="0"
-          src="https://observablehq.com/embed/@stardisblue/showcase?cells=viewof+showcase"
-        />
-      </Section>
-      <Section>
-        <h2>👨‍🏫 Expérience</h2>
-        <Experiences.Comwatt />
-        <Experiences.IndepMood />
-        <Experiences.Doctorat />
-        <Experiences.StagesRecherche />
-        <Experiences.Wbs />
-        <Experiences.IndepPc />
-      </Section>
-      <Section>
-        <h2>🎓 Éducation</h2>
-        <Educations />
-      </Section>
-      <Section>
-        <h2>👨‍🏫 Service Académique</h2>
-        <div className="grid gap-y-4 lg:grid-cols-2 ">
-          <article>
-            <h3 className="mt-0">Comité d'organisation</h3>
-            <AcademicServices.OrganisationCommitee />
-          </article>
-          <article>
-            <h3 className="mt-0">Reviewer</h3>
-            <div className="prose">
+    <div className="prose max-w-none">
+      <main id="CF" className="ph2-m container mx-auto divide-y">
+        <header className="py-4">
+          <h1>Fati CHEN</h1>
+          <h2>Docteur en Informatique, Data Science & Data Viz</h2>
+          <div>
+            <Link href="//stardis.blue">stardis.blue</Link>
+            {' · '}
+            <Link href="mailto:chen.fati@gmail.com" icon={Email}>
+              chen.fati@gmail.com
+            </Link>
+            {' · '}
+            <GithubLink href="stardisblue">stardisblue</GithubLink>
+            {' · '}
+            <ObservableLink href="@stardisblue">@stardisblue</ObservableLink>
+            {' · '}
+            <LinkedInLink href="in/fati-chen/">Fati Chen</LinkedInLink>
+          </div>
+        </header>
+        <Section>
+          <h2>📰 News</h2>
+          <iframe
+            width="100%"
+            height="319"
+            frameBorder="0"
+            src="https://observablehq.com/embed/@stardisblue/showcase?cells=viewof+showcase"
+          />
+        </Section>
+        <Section>
+          <h2>👨‍🏫 Expérience</h2>
+          <Experiences.Comwatt />
+          <Experiences.IndepMood />
+          <Experiences.Doctorat />
+          <Experiences.StagesRecherche />
+          <Experiences.Wbs />
+          <Experiences.IndepPc />
+        </Section>
+        <Section>
+          <h2>🎓 Éducation</h2>
+          <Educations />
+        </Section>
+        <Section>
+          <h2>👨‍🏫 Service Académique</h2>
+          <div className="grid gap-y-4 lg:grid-cols-2 ">
+            <article>
+              <h3 className="mt-0">Comité d'organisation</h3>
+              <AcademicServices.OrganisationCommitee />
+            </article>
+            <article>
+              <h3 className="mt-0">Reviewer</h3>
               <p>
                 <Link href="https://www.computer.org/csdl/journal/tg" iconless>
                   TVCG 2023
@@ -75,30 +75,30 @@ export default function Home() {
                 </em>
                 , 1 paper.
               </p>
-            </div>
+            </article>
+          </div>
+          <article>
+            <h3>Enseignements</h3>
+            <AcademicServices.Teachings />
           </article>
-        </div>
-        <article>
-          <h3>Enseignements</h3>
-          <AcademicServices.Teachings />
-        </article>
-        <article>
-          <h3>Encadrement</h3>
-          <AcademicServices.AlexisSupervision />
-        </article>
-      </Section>
-      <Section>
-        <h2>📡 Communications</h2>
-        <article>
-          <h3 className="mt-0">🖥️ Présentations</h3>
-          <Communications.Presentations />
-        </article>
-        <article>
-          <h3>📄 Publications</h3>
-          <Communications.Publications />
-        </article>
-      </Section>
-      <Footer />
-    </main>
+          <article>
+            <h3>Encadrement</h3>
+            <AcademicServices.AlexisSupervision />
+          </article>
+        </Section>
+        <Section>
+          <h2>📡 Communications</h2>
+          <article>
+            <h3 className="mt-0">🖥️ Présentations</h3>
+            <Communications.Presentations />
+          </article>
+          <article>
+            <h3>📄 Publications</h3>
+            <Communications.Publications />
+          </article>
+        </Section>
+        <Footer />
+      </main>
+    </div>
   );
 }
