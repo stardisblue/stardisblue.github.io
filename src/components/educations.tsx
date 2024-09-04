41a33a2e0b34d27065e1aa98cf29cfde58995b5a
stardisblue <stardisblue@gmail.com>
2024-09-04 22:31:30 +0200

    refactor: remove EducationMetaData in favor of MetaData

--- src/components/educations.tsx
+++ src/components/educations.tsx
@@ -1,44 +1,28 @@
-import { Time, Place, Link } from './ui';
+import { Link, MetaData } from './ui';
 import { UM } from './places';
-
-function EducationMetaData({
-  date,
-  place,
-}: {
-  date: string;
-  place: React.ReactNode;
-}) {
-  return (
-    <small>
-      <i>
-        <Time date={date} form="y" />
-      </i>{' '}
-      <Place>{place}</Place>
-    </small>
-  );
-}
 
 export function Educations() {
   return (
     <div className="grid grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-none lg:auto-cols-[minmax(max-content,_1fr)] lg:grid-flow-col lg:grid-rows-1 lg:snap-x lg:snap-mandatory lg:overflow-x-auto gap-4">
       <article className="sm:border-l-2 sm:pl-4">
-        <EducationMetaData date="2022" place={<UM />} />
+        <MetaData date="2022" dateFormat="y" place={<UM />} />
         <h3 className="mt-0">Doctorat Informatique</h3>
         <h4 className="mt-0">Data visualisation &amp; Data science</h4>
       </article>
       <article className="sm:border-l-2 sm:pl-4">
-        <EducationMetaData date="2018" place={<UM />} />
+        <MetaData date="2018" dateFormat="y" place={<UM />} />
         <h3 className="mt-0">Master Informatique</h3>
         <h4 className="mt-0">Architecture logicielle</h4>
       </article>
       <article className="sm:border-l-2 sm:pl-4">
-        <EducationMetaData date="2016" place={<UM />} />
+        <MetaData date="2016" dateFormat="y" place={<UM />} />
         <h3 className="mt-0">Licence Informatique</h3>
         <h4 className="mt-0">Architecture logicielle</h4>
       </article>
       <article className="sm:border-l-2 sm:pl-4">
-        <EducationMetaData
+        <MetaData
           date="2015"
+          dateFormat="y"
           place={
             <Link
               href="https://iut-montpellier-sete.edu.umontpellier.fr/"
