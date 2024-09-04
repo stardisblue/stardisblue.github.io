22a6d2815e415c0b3d5693ad65017bbff28275d7
stardisblue <stardisblue@gmail.com>
2024-09-04 20:41:36 +0200

    refactor: set heading as global styles

--- src/components/ui/typography/Heading.tsx
+++ src/components/ui/typography/Heading.tsx
@@ -1,56 +1,30 @@
-import classNames from 'classnames';
 import { HTMLAttributes } from 'react';
 
 const Heading = ({
   variant: Variant,
-  className,
   ...props
 }: {
   variant: React.ElementType<
     HTMLAttributes<HTMLHeadingElement>,
     'h1' | 'h2' | 'h3' | 'h4'
   >;
-} & React.HTMLAttributes<HTMLHeadingElement>) => (
-  <Variant
-    className={classNames('text-pretty', 'font-serif', className)}
-    {...props}
-  />
-);
+} & React.HTMLAttributes<HTMLHeadingElement>) => <Variant {...props} />;
 
-export const Heading1 = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLHeadingElement>) =>
+/** @deprecated use <h1> */
+export const Heading1 = (props: React.HTMLAttributes<HTMLHeadingElement>) =>
   Heading({
     variant: 'h1',
-    className: classNames(
-      'font-extrabold',
-      'text-[2.25em]/[1.11]',
-      'mt-0',
-      'mb-[0.89em]',
-      'small-caps',
-      className
-    ),
     ...props,
   });
 
-export const Heading2 = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLHeadingElement>) =>
+/** @deprecated use <h2>  */
+export const Heading2 = (props: React.HTMLAttributes<HTMLHeadingElement>) =>
   Heading({
     variant: 'h2',
-    className: classNames(
-      'font-bold',
-      'text-[1.5em]/[1.33]',
-      'mt-[2em]',
-      'mb-[1em]',
-      'small-caps',
-      className
-    ),
     ...props,
   });
 
+/** @deprecated  use <h3>  */
 export const Heading3 = ({
   className,
   mt0 = false,
@@ -58,17 +32,11 @@ export const Heading3 = ({
 }: { mt0?: boolean } & React.HTMLAttributes<HTMLHeadingElement>) =>
   Heading({
     variant: 'h3',
-    className: classNames(
-      'font-semibold',
-      'text-[1.25em]/[1.6]',
-      mt0 ? 'mt0' : 'mt-[1.6em]',
-      'mb-[0.6em]',
-      'small-caps',
-      className
-    ),
+    className: classNames({ 'mt-0': mt0 }, className),
     ...props,
   });
 
+/** @deprecated  use <h4>  */
 export const Heading4 = ({
   className,
   mt0 = false,
@@ -76,12 +44,6 @@ export const Heading4 = ({
 }: { mt0?: boolean } & React.HTMLAttributes<HTMLHeadingElement>) =>
   Heading({
     variant: 'h4',
-    className: classNames(
-      'font-semibold',
-      mt0 ? 'mt0' : 'mt-[1.5em]',
-      'mb-[0.5em]',
-      'leading-[1.5]',
-      className
-    ),
+    className: classNames({ 'mt-0': mt0 }, className),
     ...props,
   });
