f29f5ccf329585a9b8f98414a97f67f7084aa89b
stardisblue <stardisblue@gmail.com>
2024-09-04 16:24:32 +0200

    feat: add ArticleMetaData

---
+++ src/components/ui/ArticleMetaData.tsx
@@ -0,0 +1,35 @@
+import { Place } from './Place';
+import { Time } from './Time';
+import { TimeInterval } from './TimeInterval';
+
+type Props = {
+  date: string | [string, string];
+  dateFormat?: string;
+  place?: React.ReactNode;
+  children?: React.ReactNode;
+};
+
+export function ArticleMetaData({ date, dateFormat, place, children }: Props) {
+  const dateNode = Array.isArray(date) ? (
+    <TimeInterval start={date[0]} end={date[1]} form={dateFormat} />
+  ) : (
+    <i>
+      <span className="sr-only">date : </span>
+      <Time date={date} form={dateFormat} />
+    </i>
+  );
+
+  return (
+    <div>
+      <small>
+        {dateNode}{' '}
+        {place && (
+          <>
+            <Place>{place}</Place>{' '}
+          </>
+        )}
+        {children}
+      </small>
+    </div>
+  );
+}
--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,6 +1,7 @@
+export { ArticleMetaData } from './ArticleMetaData';
 export { Keywords } from './Keywords';
-export { Place } from './Place';
 export { Links } from './Links';
+export { Place } from './Place';
 export * from './Section';
 export { Time } from './Time';
 export * from './TimeInterval';
