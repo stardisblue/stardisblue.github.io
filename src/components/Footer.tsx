27f67b616a6fcc9c5bdf1d8aad7e0b3d4f40ad62
stardisblue <stardisblue@gmail.com>
2024-09-07 20:20:39 +0200

    fix (Footer): bad url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,8 @@ import { GithubLink } from './ui';
 export function Footer() {
   return (
     <footer>
-      2024 &mdash;{' '}
-      <GithubLink href="stardisblue.github.io">
-        stardisblue.github.io
-      </GithubLink>{' '}
-      Made with 💖 &amp; Next.js
+      2024 &mdash; Made with 💖 &amp; Next.js.{' '}
+      <GithubLink href="stardisblue/stardisblue.github.io">source</GithubLink>
     </footer>
   );
 }
