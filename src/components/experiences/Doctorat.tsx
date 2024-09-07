a2b1c61d1e79880c7ce19ff4975221c60a912acb
stardisblue <stardisblue@gmail.com>
2024-09-07 20:12:31 +0200

    feat: add images to Doctorat

--- src/components/experiences/Doctorat.tsx
+++ src/components/experiences/Doctorat.tsx
@@ -8,11 +8,17 @@ import {
 } from '@/components/ui';
 import { Document, Presentation } from '@/components/ui/icons';
 import { Lirmm } from '../places';
+import ExportedImage from 'next-image-export-optimizer';
 
+import prosovisImage from './doctorat_prosovis.png';
+import agoraImage from './doctorat_agora.png';
+import fsacImage from './doctorat_fsac.png';
 export function Doctorat() {
   return (
-    <article className="max-w-prose">
-      <h3>Doctorat en Data Science &amp; Data Visualisation</h3>
+    <article>
+      <h3 className="max-w-prose">
+        Doctorat en Data Science &amp; Data Visualisation
+      </h3>
       <Place>
         <Lirmm city />
       </Place>
@@ -20,31 +26,75 @@ export function Doctorat() {
         <Links links={[<Thesis />, <Soutenance />]} />{' '}
         <Tags>R&D, Data Science, Data Viz, Web</Tags>
       </MetaData>
-      <p>
+      <p className="max-w-prose">
         Recherche dans le domaine de la visualisation analytique axée autour de
-        trois thématiques:
+        trois thématiques :
       </p>
-      <ol>
-        <li>réduction de l'encombrement visuel</li>
-        <li>agglomération spatiale d'entités et</li>
-        <li>représentations interactives de données spatio-temporelles.</li>
-      </ol>
-      <p>
-        <GithubLink href="stardisblue/prosovis">Prosovis</GithubLink> :
-        plateforme web de visualisation analytique de données prosopographiques.
-      </p>
-      <p>
-        <Link href="//agorajs.github.io" icon>
-          AGORA
-        </Link>{' '}
-        : analyse comparative des différents algorithmes de placement de
-        graphes.
-      </p>
-      <p>
-        <GithubLink href="stardisblue/fsac">FSAC</GithubLink> : agglomération
-        spatiale pour la visualisation de grand volumes de données en temps
-        réel.
-      </p>
+      <div className="lg:grid lg:grid-cols-3 lg:gap-4">
+        <div>
+          <h4 className="md:mt-0 max-w-prose">
+            ProsoVis : plateforme de visualisation analytique
+          </h4>
+          <small className="metadata mb-1 block">
+            <GithubLink href="stardisblue/prosovis">Prosovis</GithubLink>
+          </small>
+
+          <figure>
+            <ExportedImage
+              className="aspect-video object-contain"
+              alt="Dashboard Prosovis"
+              src={prosovisImage}
+            />
+            <figcaption>fig. : aperçu du dashboard</figcaption>
+          </figure>
+          <p className="md:mb-0 max-w-prose">
+            Plateforme web de visualisation analytique de données
+            prosopographiques.
+          </p>
+        </div>
+        <div>
+          <h4 className="md:mt-0 max-w-prose">
+            AGORA : comparaison de méthode de graph drawing
+          </h4>
+          <small className="metadata mb-1 block">
+            <Link href="//agorajs.github.io" icon>
+              AGORA
+            </Link>
+          </small>
+          <figure>
+            <ExportedImage
+              className="aspect-video object-contain"
+              alt="AGORAjs: page de résultats"
+              src={agoraImage}
+            />
+            <figcaption>fig. : aperçu de la page des résultats</figcaption>
+          </figure>
+          <p className="md:mb-0 max-w-prose">
+            Analyse comparative des différents algorithmes de placement de
+            graphes.
+          </p>
+        </div>
+        <div>
+          <h4 className="md:mt-0 max-w-prose">
+            FSAC : agglomération de données spatiales
+          </h4>
+          <small className="metadata mb-1 block">
+            <GithubLink href="stardisblue/fsac">FSAC</GithubLink>
+          </small>
+          <figure>
+            <ExportedImage
+              className="aspect-video object-contain"
+              alt="Exemple d'agglomération de données"
+              src={fsacImage}
+            />
+            <figcaption>fig. : exemple d'agglomération</figcaption>
+          </figure>
+          <p className="md:mb-0 max-w-prose">
+            Agglomération spatiale pour la visualisation de grand volumes de
+            données en temps réel.
+          </p>
+        </div>
+      </div>
     </article>
   );
 }
